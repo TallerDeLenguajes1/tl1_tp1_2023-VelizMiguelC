41c4f4d18916407d0d0b50062d6586150a1f05bd
VelizMiguelC <velizmiguel934@gmail.com>
2023-03-16 23:46:00 +0100

    cambios

--- tp1_1.c
+++ tp1_1.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
 int main(){
     printf("Hola mundo");
+    int *punt;
+    int x;
+    x=1;
+    punt=&x;
+    printf("La dirección de memoria almacenada por el puntero:%d\n",punt);
+    printf("la dirección de memoria de la variable:%d\n",*punt);
+    printf("la dirección de memoria del puntero: %d\n",&punt);
+    printf("El tamaño de memoria utilizado por esa variable usando la función sizeof():%d",sizeof(x));
+    printf("El contenido del puntero:%d\n",punt);
+    
+
+
+    return 0;
 }
